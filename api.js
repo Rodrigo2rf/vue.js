148f330e8fa8e3b855fd53ecb3d11966d17f7f45
Rodrigo.reinaldo <rodrigo.2rf@gmail.com>
2018-06-08 14:40:32 -0300

    Primeiro commit

---
+++ api.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Olá Vue!'
+    }
+})
+var app_3 = new Vue({
+    el: '#app-3',
+    data:{
+        exist: true,
+        message: 'Opa eu existo!'
+    }
+})
